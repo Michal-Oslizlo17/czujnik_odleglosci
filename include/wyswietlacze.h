079329c7edbe729971d2501f7f3da501cc7bf5a1
Michael Oslizlo <119759146+VermiNew@users.noreply.github.com>
2023-08-21 11:26:53 +0200

    Przeniesienie

--- include/wyswietlacze.h
+++ include/wyswietlacze.h
@@ -1,1 +1,19 @@
+#ifndef WYSWIETLACZE
+#define WYSWIETLACZE
 
+void wyswietlacz_segment_on_off(uint8_t);
+void wyswietlacz_liczba_jeden(uint8_t);
+void wyswietlacz_liczba_dwa(uint8_t);
+void wyswietlacz_liczba_trzy(uint8_t);
+void wyswietlacz_liczba_cztery(uint8_t);
+void wyswietlacz_segment_a(uint8_t);
+void wyswietlacz_segment_b(uint8_t);
+void wyswietlacz_segment_c(uint8_t);
+void wyswietlacz_segment_d(uint8_t);
+void wyswietlacz_segment_e(uint8_t);
+void wyswietlacz_segment_f(uint8_t);
+void wyswietlacz_segment_g(uint8_t);
+void wyswietlacz_segment_on_off(uint8_t);
+void wlacz_wyswietlacz(uint8_t, uint8_t, uint8_t);
+
+#endif /* WYSWIETLACZE */
