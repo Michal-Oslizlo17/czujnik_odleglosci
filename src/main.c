9b4f6513b085732831a6fe6d05904282b6643451
Michael Oslizlo <119759146+VermiNew@users.noreply.github.com>
2023-08-21 11:20:04 +0200

    Nie mam żadnej pewności czy nie jestem głupi

--- src/main.c
+++ src/main.c
@@ -52,6 +52,57 @@ int main(void)
         //     USART1_SendByte('\n');
         //     odczytano = 0;
         // }
+        // Jeśli odczytano dane, przetwórz je i wyświetl na wyświetlaczach
+if (odczytano == 1)
+{
+    ftoa(odleglosc_cm, wynik_cstring, 2);
+    USART1_SendCString(wynik_cstring, 10);
+    USART1_SendByte('\n');
+
+    char *comma_pos = strchr(wynik_cstring, ','); // Znajdź pozycję przecinka
+
+    if (comma_pos != NULL)
+    {
+        *comma_pos = '\0'; // Zamień przecinek na znak zakończenia łańcucha [NIE MAM INNEGO POMYSŁU - CZASEM MA TO PROBLEM Z DZIAŁANIEM]
+
+        int distance_int = atoi(wynik_cstring); // Przetwórz część przed przecinkiem na liczbę całkowitą
+        int fraction_int = atoi(comma_pos + 1);  // Przetwórz część po przecinku na liczbę całkowitą
+
+        // Wyłącz wszystkie wyświetlacze
+        wlacz_wyswietlacz(0, 1, 1);
+        wlacz_wyswietlacz(0, 2, 1);
+        wlacz_wyswietlacz(0, 3, 1);
+        wlacz_wyswietlacz(0, 4, 1);
+
+        // Podziel liczbę przed przecinkiem na poszczególne cyfry i wyświetl na odpowiednich wyświetlaczach
+        int digit = 0;
+        int number = distance_int;
+        int numer_wyswietlacza = 1; // Numer pierwszego wyświetlacza
+
+        while (number > 0 && numer_wyswietlacza <= 4)
+        {
+            digit = number % 10;
+            number /= 10;
+            wlacz_wyswietlacz(digit, numer_wyswietlacza, 1);
+            numer_wyswietlacza++;
+        }
+
+        // Przetwórz część po przecinku i wyświetl ją na odpowiednich wyświetlaczach
+        number = fraction_int;
+        numer_wyswietlacza = 1; // Numer pierwszego wyświetlacza dla części po przecinku
+
+        while (number > 0 && numer_wyswietlacza <= 2) // Dla dwóch cyfr po przecinku
+        {
+            digit = number % 10;
+            number /= 10;
+            // wlacz_wyswietlacz(digit, numer_wyswietlacza + 2); // Wyświetlacz 3 i 4
+            // numer_wyswietlacza++;
+        }
+    }
+
+    odczytano = 0; // Zresetuj flagę odczytu
+}
+      }
 wyswietlacz_liczba_jeden(0);
 wyswietlacz_liczba_dwa(0);
 wyswietlacz_liczba_trzy(0);
