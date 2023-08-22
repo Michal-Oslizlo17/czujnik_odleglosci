c74f728ae53f3e148afa3012f4a01e316b3ae7d5
Michael Oslizlo <119759146+VermiNew@users.noreply.github.com>
2023-08-22 10:55:33 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include "stm32f103xb.h" // Inicjalizacja zmiennej wskaźnika opóźnienia
+#include "stm32f103xb.h"
 #include "timery.h"
 #include "uart.h"
 #include "ftoa.h"
@@ -14,8 +14,8 @@ int buffer_index = 0;
 // Funkcja główna
 int main(void)
 {
-    SysTick_Config(4095);                         // Domyślnie: SysTick_Config(8000);
-    SysTick->CTRL &= ~SysTick_CTRL_CLKSOURCE_Msk; // 8000000/8
+    SysTick_Config(4095);
+    SysTick->CTRL &= ~SysTick_CTRL_CLKSOURCE_Msk;
 
     // Inicjalizacja przycisku
     // button_init();
@@ -53,104 +53,101 @@ int main(void)
         //     USART1_SendByte('\n');
         //     odczytano = 0;
         // }
+        
         // Jeśli odczytano dane, przetwórz je i wyświetl na wyświetlaczach
-if (odczytano == 1)
-{
-    ftoa(odleglosc_cm, wynik_cstring, 2);
-    USART1_SendCString(wynik_cstring, 10);
-    USART1_SendByte('\n');
-
-    char *comma_pos = strchr(wynik_cstring, ','); // Znajdź pozycję przecinka
-
-    if (comma_pos != NULL)
-    {
-        *comma_pos = '\0'; // Zamień przecinek na znak zakończenia łańcucha [NIE MAM INNEGO POMYSŁU - CZASEM MA TO PROBLEM Z DZIAŁANIEM]
-
-        int distance_int = atoi(wynik_cstring); // Przetwórz część przed przecinkiem na liczbę całkowitą
-        int fraction_int = atoi(comma_pos + 1);  // Przetwórz część po przecinku na liczbę całkowitą
-
-        // Wyłącz wszystkie wyświetlacze
-        wlacz_wyswietlacz(0, 1, 0);
-        wlacz_wyswietlacz(0, 2, 0);
-        wlacz_wyswietlacz(0, 3, 0);
-        wlacz_wyswietlacz(0, 4, 0);
-
-        // Podziel liczbę przed przecinkiem na poszczególne cyfry i wyświetl na odpowiednich wyświetlaczach
-        int digit = 0;
-        int number = distance_int;
-        int numer_wyswietlacza = 1; // Numer pierwszego wyświetlacza
-
-        while (number > 0 && numer_wyswietlacza <= 4)
+        if (odczytano == 1)
         {
-            digit = number % 10;
-            number /= 10;
-            wlacz_wyswietlacz(digit, numer_wyswietlacza, 1);
-            numer_wyswietlacza++;
+            ftoa(odleglosc_cm, wynik_cstring, 2);
+            USART1_SendCString(wynik_cstring, 10);
+            USART1_SendByte('\n');
+
+            char *comma_pos = strchr(wynik_cstring, ',');
+            
+            if (comma_pos != NULL)
+            {
+                *comma_pos = '\0';
+
+                int distance_int = atoi(wynik_cstring);
+                int fraction_int = atoi(comma_pos + 1);
+
+                wlacz_wyswietlacz(0, 1, 0);
+                wlacz_wyswietlacz(0, 2, 0);
+                wlacz_wyswietlacz(0, 3, 0);
+                wlacz_wyswietlacz(0, 4, 0);
+
+                int digit = 0;
+                int number = distance_int;
+                int numer_wyswietlacza = 1;
+
+                while (number > 0 && numer_wyswietlacza <= 4)
+                {
+                    digit = number % 10;
+                    number /= 10;
+                    wlacz_wyswietlacz(digit, numer_wyswietlacza, 1);
+                    numer_wyswietlacza++;
+                }
+
+                number = fraction_int;
+                numer_wyswietlacza = 1;
+
+                while (number > 0 && numer_wyswietlacza <= 2)
+                {
+                    digit = number % 10;
+                    number /= 10;
+                    // wlacz_wyswietlacz(digit, numer_wyswietlacza + 2);
+                    // numer_wyswietlacza++;
+                }
+            }
+
+            odczytano = 0;
         }
 
-        // Przetwórz część po przecinku i wyświetl ją na odpowiednich wyświetlaczach
-        number = fraction_int;
-        numer_wyswietlacza = 1; // Numer pierwszego wyświetlacza dla części po przecinku
+        // wyswietlacz_liczba_jeden(0);
+        // wyswietlacz_liczba_dwa(0);
+        // wyswietlacz_liczba_trzy(0);
+        // wyswietlacz_liczba_cztery(0);
 
-        while (number > 0 && numer_wyswietlacza <= 2) // Dla dwóch cyfr po przecinku
-        {
-            digit = number % 10;
-            number /= 10;
-            // wlacz_wyswietlacz(digit, numer_wyswietlacza + 2); // Wyświetlacz 3 i 4
-            // numer_wyswietlacza++;
-        }
-    }
+        // wyswietlacz_liczba_jeden(1);
+        // wyswietlacz_segment_a(1);
+        // wyswietlacz_segment_b(1);
+        // wyswietlacz_segment_c(1);
+        // wyswietlacz_segment_d(1);
+        // wyswietlacz_segment_e(1);
+        // wyswietlacz_segment_f(1);
+        // wyswietlacz_segment_g(1);
+        // wyswietlacz_liczba_jeden(0);
 
-    odczytano = 0; // Zresetuj flagę odczytu
-}
-      }
-wyswietlacz_liczba_jeden(0);
-wyswietlacz_liczba_dwa(0);
-wyswietlacz_liczba_trzy(0);
-wyswietlacz_liczba_cztery(0);
+        // wyswietlacz_liczba_dwa(1);
+        // wyswietlacz_segment_a(1);
+        // wyswietlacz_segment_b(1);
+        // wyswietlacz_segment_c(1);
+        // wyswietlacz_segment_d(1);
+        // wyswietlacz_segment_e(1);
+        // wyswietlacz_segment_f(1);
+        // wyswietlacz_segment_g(1);
+        // wyswietlacz_liczba_dwa(0);
 
-        wyswietlacz_liczba_jeden(1);
-        wyswietlacz_segment_a(1);
-        wyswietlacz_segment_b(1);
-        wyswietlacz_segment_c(1);
-        wyswietlacz_segment_d(1);
-        wyswietlacz_segment_e(1);
-        wyswietlacz_segment_f(1);
-        wyswietlacz_segment_g(1);
-        wyswietlacz_liczba_jeden(0);
+        // wyswietlacz_liczba_trzy(1);
+        // wyswietlacz_segment_a(1);
+        // wyswietlacz_segment_b(1);
+        // wyswietlacz_segment_c(1);
+        // wyswietlacz_segment_d(1);
+        // wyswietlacz_segment_e(1);
+        // wyswietlacz_segment_f(1);
+        // wyswietlacz_segment_g(1);
+        // wyswietlacz_liczba_trzy(0);
 
-        wyswietlacz_liczba_dwa(1);
-        wyswietlacz_segment_a(1);
-        wyswietlacz_segment_b(1);
-        wyswietlacz_segment_c(1);
-        wyswietlacz_segment_d(1);
-        wyswietlacz_segment_e(1);
-        wyswietlacz_segment_f(1);
-        wyswietlacz_segment_g(1);
-        wyswietlacz_liczba_dwa(0);
-
-        wyswietlacz_liczba_trzy(1);
-        wyswietlacz_segment_a(1);
-        wyswietlacz_segment_b(1);
-        wyswietlacz_segment_c(1);
-        wyswietlacz_segment_d(1);
-        wyswietlacz_segment_e(1);
-        wyswietlacz_segment_f(1);
-        wyswietlacz_segment_g(1);
-        wyswietlacz_liczba_trzy(0);
-
-        wyswietlacz_liczba_cztery(1);
-        wyswietlacz_segment_a(1);
-        wyswietlacz_segment_b(1);
-        wyswietlacz_segment_c(1);
-        wyswietlacz_segment_d(1);
-        wyswietlacz_segment_e(1);
-        wyswietlacz_segment_f(1);
-        wyswietlacz_segment_g(1);
-        wyswietlacz_liczba_cztery(0);
+        // wyswietlacz_liczba_cztery(1);
+        // wyswietlacz_segment_a(1);
+        // wyswietlacz_segment_b(1);
+        // wyswietlacz_segment_c(1);
+        // wyswietlacz_segment_d(1);
+        // wyswietlacz_segment_e(1);
+        // wyswietlacz_segment_f(1);
+        // wyswietlacz_segment_g(1);
+        // wyswietlacz_liczba_cztery(0);
     }
 }
-
 
 // TODO
 
